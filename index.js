103b903814535a9bac37586c5154f613959dbb88
J-Agens <johntagens@gmail.com>
2019-04-26 12:03:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(userName, userEmail) {
+  let formData = {
+    name: userName,
+    email: userEmail
+  };
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      document.body.textContent = object.id.toString();
+    })
+    .catch(function(error) {
+      alert(error.message);
+      console.log(error.message)
+    });
+}
